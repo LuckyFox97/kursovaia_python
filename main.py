637db124395ff4cb62c53c8a23d35f863d628f0f
Dmitry <dima12_yarrin@mail.ru>
2024-04-26 01:10:49 +0300

    fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from main_proj.main import OperationsProcessor
 
+
 # Создаем экземпляр класса и обрабатываем данные
 processor = OperationsProcessor()
 processor.date_check()
@@ -8,3 +9,5 @@ processor.description_operation()
 # Выводим отформатированные данные последних 5 выполненных транзакций
 formatted_transactions = processor.masked_operation()
 print(formatted_transactions)
+
+
